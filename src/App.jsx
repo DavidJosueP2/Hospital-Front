93a8d14bf76fefc036817c649121ec5d2da819d8
Josue Joel Garcia Abata <134705823+JosueGarciaAbata@users.noreply.github.com>
2025-09-23 07:16:12 -0500

    Revert "feat: implement login with DNI/email, route protection and backend auto-reset"

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,23 @@ import AdminLayout from "@/layouts/AdminLayout";
 import Playground from "@/pages/admin/Playground";
 import AppToaster from "@/inc/ui/Toaster.jsx";
 import NotFound from "@/pages/NotFound.jsx";
-import AuthLayout from "@/pages/auth/AuthLayour";
-import Login from "@/pages/auth/Login";
-import ErrorBoundary from "@/utils/ErrorBoundary";
-import { AuthProvider } from "@/context/AuthContext";
-import { ProtectedRoute } from "@/utils/ProtectedRoute";
 
 export default function App() {
-  return (
-    <ErrorBoundary>
-      <AuthProvider>
-        <Routes>
-          {/* Auth */}
-          <Route element={<AuthLayout />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
+    return (
+        <>
+            <Routes>
+                {/* Rutas dentro del layout de administración */}
+                <Route element={<AdminLayout />}>
+                    <Route index element={<Navigate to="/playground" replace />} />
+                    <Route path="/playground" element={<Playground />} />
+                </Route>
 
-          {/* Rutas dentro del layout de administración */}
-          <Route element={<ProtectedRoute />}>
-            <Route element={<AdminLayout />}>
-              <Route index element={<Navigate to="/playground" replace />} />
-              <Route path="/playground" element={<Playground />} />
-            </Route>
-          </Route>
+                {/* Fallback público */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
 
-          {/* Fallback público */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-        {/* Toaster global (fuera de <Routes>) */}
-        <AppToaster />
-      </AuthProvider>
-    </ErrorBoundary>
-  );
+            {/* Toaster global (fuera de <Routes>) */}
+            <AppToaster />
+        </>
+    );
 }
